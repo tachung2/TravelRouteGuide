696ddcd7f32d7015b1afaf5ee1b9ad50d5063c70
20191792 <20191792@edu.hanbat.ac.kr>
2022-06-23 06:43:40 +0900

    update: main page and resources

--- app/src/main/java/com/example/trg/MainActivity.java
+++ app/src/main/java/com/example/trg/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_page);
 
         MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
