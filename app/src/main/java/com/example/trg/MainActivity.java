30ed33f5ba05f148b61a1e5aaa11f42becd25061
20191792 <20191792@edu.hanbat.ac.kr>
2022-06-23 01:06:04 +0900

    update: login page, sign up page, kakao map api

---
+++ app/src/main/java/com/example/trg/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.trg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+    }
+}
