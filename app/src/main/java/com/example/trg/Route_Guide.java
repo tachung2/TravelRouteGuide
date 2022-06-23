5ebf7101c15a2f2602be7de753c9ac849ade3f7f
tachung2 <tachung@naver.com>
2022-06-23 22:35:52 +0900

    commit

--- app/src/main/java/com/example/trg/Route_Guide.java
+++ app/src/main/java/com/example/trg/Route_Guide.java
@@ -1,13 +1,24 @@
 package com.example.trg;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Route_Guide extends AppCompatActivity {
+import net.daum.mf.map.api.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.route_guide);
+        setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
+
 }
